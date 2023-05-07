a4cb85e5fe77c7df4e0b91b3492a54c241584c53
dhewan <dhewanagumm@gmail.com>
2023-05-07 18:57:40 +0700

    merge 2

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 router.use('/users', require('./user'))
 router.use('/photos', require('./photo'));
 router.use("/comments", require("./comment"));
+router.use('/socialmedias', require('./sosmed'));
 
 module.exports = router;
---
+++ routes/sosmed.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+const auth = require('../middlewares/auth')
+const sosmed = require('../controllers/socialMedia')
+
+router.post('/', auth,sosmed.post)
+router.get('/', auth,sosmed.get)
+router.put('/:id', auth,sosmed.put)
+router.delete('/:id', auth,sosmed.delete)
+
+module.exports = router;
+
