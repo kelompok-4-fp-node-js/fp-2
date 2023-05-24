73704cd3a3e8b26fd457957cc6160673ac9ee7cf
Kharis <Kharisrahmat686@gmail.com>
2023-05-24 11:44:31 +0700

    adjustment / endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-router.use('/users', require('./user'))
-router.use('/photos', require('./photo'));
+router.get("/", (req, res) => {
+  res.json({ message: "welcome to Toko belanja Kelompok 4" });
+});
+router.use("/users", require("./user"));
+router.use("/photos", require("./photo"));
 router.use("/comments", require("./comment"));
-router.use('/socialmedias', require('./sosmed'));
+router.use("/socialmedias", require("./sosmed"));
 
 module.exports = router;
